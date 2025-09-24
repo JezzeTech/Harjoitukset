bab5da49d6452c2f6fab4c4b64378662b93467e3
jesvilju <jesse.v.raatikainen@student.jyu.fi>
2025-09-24 15:53:05 +0300

    notejen_lisays_ja_pyynto_tietokantaan

--- NodeJs/index.js
+++ NodeJs/index.js
@@ -1,8 +1,7 @@
 require('dotenv').config()
 const express = require('express')
-const Note = require('./models.note')
-const app = express()
 const Note = require('./models/note')
+const app = express()
 
 app.use(express.json())
 app.use(express.static('dist'))
@@ -28,11 +27,7 @@ let notes = [
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
-/*
-app.get('/api/notes', (request, response) => {
-  response.json(notes)
-})
-*/
+
 app.get('/api/notes', (request, response) => {
   Note.find({}).then(notes => {
     response.json(notes)
@@ -40,41 +35,28 @@ app.get('/api/notes', (request, response) => {
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  const note = notes.find(note => note.id === id)
-
-  if (note) {
+  Note.findById(request.params.id).then(note => {
     response.json(note)
-  } else {
-    response.status(404).end()
-  }
+  })
 })
-
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => Number(n.id)))
-    : 0
-  return String(maxId + 1)
-}
 
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
   if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
+    return response.status(400).json({
+      error: 'content missing'
     })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generateId(),
-  }
+  })
 
-  notes = notes.concat(note)
-
-  response.json(note)
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -88,5 +70,3 @@ const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
-
-const http = require('http')
