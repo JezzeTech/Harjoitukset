b5f9d95c074bff3c17828d2178f3e3dcf02f8118
jesvilju <jesse.v.raatikainen@student.jyu.fi>
2025-09-24 15:14:44 +0300

    tietokannan_lisaaminen

--- NodeJs/index.js
+++ NodeJs/index.js
@@ -1,5 +1,8 @@
+require('dotenv').config()
 const express = require('express')
+const Note = require('./models.note')
 const app = express()
+const Note = require('./models/note')
 
 app.use(express.json())
 app.use(express.static('dist'))
@@ -25,9 +28,15 @@ let notes = [
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
-
+/*
 app.get('/api/notes', (request, response) => {
   response.json(notes)
+})
+*/
+app.get('/api/notes', (request, response) => {
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
 app.get('/api/notes/:id', (request, response) => {
@@ -75,7 +84,7 @@ app.delete('/api/notes/:id', (request, response) => {
   response.status(204).end()
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
